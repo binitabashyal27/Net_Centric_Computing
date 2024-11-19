e755524bcfb356e2a342d77ffaeeccf809becbd9
binitabashyal27 <142423058+binitabashyal27@users.noreply.github.com>
2024-11-19 15:08:35 +0545

    Add files via upload

---
+++ swap_Number/swap_Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 5, secondNumber = 10;
+            Console.WriteLine("Before Swapping:");
+            Console.WriteLine("firstNumber= " + firstNumber);
+            Console.WriteLine("secondNumber= " + secondNumber);
+
+            //Swap using mathematical operations
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+
+            Console.WriteLine("After Swapping:");
+            Console.WriteLine("firstNumber= " + firstNumber);
+            Console.WriteLine("secondNumber= " + secondNumber);
+
+
+
+
+        }
+    }
+}
